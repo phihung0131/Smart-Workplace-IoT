d73619e87450112153cb989b4efa787bb6ce4fa2
phihung0131 <phihung0131@gmail.com>
2024-11-12 00:26:35 +0700

    dd

--- Gateway/config.py
+++ Gateway/config.py
@@ -1,6 +1,6 @@
 CONFIG = {
     'ADAFRUIT_USERNAME': 'phihung0131',
-    'ADAFRUIT_KEY': 'aio_ezmO15hBfxguQaKBDEMx4xAPEUOE',
+    'ADAFRUIT_KEY': '',
     'FEED_IDS': ['temperature', 'humidity', 'light', 'user', 'led', 'fan', 'auto_mode'],
     'ACCESS_KEY_PICOVOICE' : 'cl+Go00EQVss5WnE9iscIq/qXoYGTfwCKVv9Di9Cr8HVQ+51zMaOhA==',
 }
